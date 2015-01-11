50201106eddea3ae18632c49880b5ebca5638691
Kevin Savage <kevin@Jos-MacBook-Air-2.local>
2015-01-11 22:02:05 +0000

    config

--- src/main/scala/Taxi.scala
+++ src/main/scala/Taxi.scala
@@ -5,27 +5,29 @@ import org.apache.spark.SparkConf
 
 object Taxi {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("taxi")
+    val conf = new SparkConf().setAppName("taxi") 
     val sc = new SparkContext(conf)
 
     val start = 1
     val end = args(0).toInt
-    val lowerLmit = args(1).toInt
+    val lowerLimit = args(1).toInt
+    val lowerBounds = BigInt(args(2))
+    val splits = args(3).toInt
     val range = Range(start, end)
 
     val cubes = sc
-      .parallelize(range, 50)
+      .parallelize(range, splits)
       .map(BigInt(_))
       .map(x => x*x*x)
-      .cache()
 
     val results = cubes
       .cartesian(cubes)
       .filter(pair => pair._1 <= pair._2)
       .map(pair => pair._1 + pair._2)
+      .filter(_ > lowerBounds)
       .map(x => (x, 1L))
       .reduceByKey(_ + _)
-      .filter(counted => counted._2 > lowerLmit)
+      .filter(counted => counted._2 > lowerLimit)
       .groupBy(counted => counted._2)
       .map(byCount => (byCount._1, byCount._2.map(_._1).min))
 
