7ea4fee557f20dc622ca1472f5164d93e251cbae
Kevin Savage <kevin.savage@mendeley.com>
2015-01-22 10:52:09 +0000

    less precise

--- src/main/scala/Taxi5.scala
+++ src/main/scala/Taxi5.scala
@@ -1,9 +1,9 @@
 import org.apache.spark.{SparkConf, SparkContext}
 
-import scala.util.Random
 import scala.math._
+import scala.util.Random
 
-object Taxi4 {
+object Taxi5 {
   def main(args: Array[String]): Unit = {
     val start = args(0).toInt
     val end = args(1).toInt
@@ -12,21 +12,17 @@ object Taxi4 {
     val splits = args(3).toInt
     val range = Random.shuffle(Range(start, end).toList)
 
-    val conf = new SparkConf().setAppName("taxi").setMaster("local[8]")
+    val conf = new SparkConf().setAppName("taxi").setMaster("local[1]")
     val sc = new SparkContext(conf)
 
     val values = sc
       .parallelize(range, splits)
 
-    val precision = pow(10, 9)
-
     val result = values.cartesian(values)
       .filter(pair => pair._1 <= pair._2)
-      .map(pair => (3*log(pair._1)) + log1p(exp(3 * (log(pair._2) - log(pair._1)))))
-      .groupBy(v => floor(v*precision)/precision)
-      .map(pair => (pair._1, pair._2.size)).filter(_._2>lowerLimitToTN)
-      .map(p=> exp(p._1))
-
+      .map(pair => Math.pow(pair._1, 3) + Math.pow(pair._2, 3))
+      .groupBy(identity)
+      .map(pair => (pair._1, pair._2.size)).filter(_._2>lowerLimitToTN).map(_._1)
 
     val collect: Array[Double] = result.collect()
     collect.foreach(println(_))
