3c018c38ca74ed3cbc35bac05242f6f958da595c
Kevin Savage <kevin@Jos-Air-2.home>
2015-02-08 22:32:04 +0000

    working out why it doesn't work with stronger limits

--- src/main/scala/Taxi8.scala
+++ src/main/scala/Taxi8.scala
@@ -1,32 +1,31 @@
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Taxi8 {
+
   def main(args: Array[String]): Unit = {
     val start = 1
     val end = args(0).toInt
 
     val lowerLimitToTN = args(1).toInt
     val splits = args(2).toInt
-    val by = 1000
-    val range = start until (end+1) by by
+    val interval = 1000
+    val range = start until (end+1) by interval
 
     val conf = new SparkConf().setAppName("taxi").setMaster("local[1]")
     val sc = new SparkContext(conf)
 
     val results = range.flatMap{ n =>
-      val maxK = Math.floor((n+by).toDouble*(Math.pow(2.0, 1.0/3.0)-1.0)).toInt + 1
+      val maxK = calculateMaxK(n, interval)
       val kRange = sc.parallelize(0 until (maxK+1), splits)
+      val lowerLimit = n*n*n*2
+      val upperLimit = (n+interval)*(n+interval)*(n+interval)*2
 
-      kRange.map{k =>
-        (k, 0, n + k)
-      }.flatMap{ case (k, minJ, maxJ) =>
-        (minJ until (maxJ)).map(j => (n + k,n - j))
-      }.filter (location => location._2>0)
-        .map{ location =>
-        val x = BigInt(location._1)
-        val y = BigInt(location._2)
-        (x*x*x)+(y*y*y)
-      }.groupBy(identity)
+      kRange.flatMap(producePairsFor(n, _))
+        .filter (location => location._2>0)
+        .map(performSumOfCubes)
+        .filter(sum => sum >= lowerLimit)
+        .filter(sum => sum < upperLimit)
+        .groupBy(identity)
         .map(group => (group._2.size, group._1))
         .filter(pair => pair._1 >= lowerLimitToTN)
         .collect()
@@ -35,6 +34,18 @@ object Taxi8 {
     results.foreach(println(_))
   }
 
+  def producePairsFor(n: Int, k: Int): TraversableOnce[(Int,Int)] = for (y <- 1 until (n+k+1)) yield (n+k, y)
+
+  def performSumOfCubes: ((Int, Int)) => BigInt = {
+    location =>
+      val x = BigInt(location._1)
+      val y = BigInt(location._2)
+      (x * x * x) + (y * y * y)
+  }
+
+  def calculateMaxK(n: Int, interval: Int): Int = {
+    Math.floor((n + interval).toDouble * (Math.pow(2.0, 1.0 / 3.0) - 1.0)).toInt + 2
+  }
 
   def calculateS(n: BigInt, k: BigInt): Int = {
     Cubes.cubeRt((k * k * k) + (k * k * n * 3) + (k * n * n * 3) - (n * n * n)).toInt
---
+++ src/main/scala/Taxi8Test.scala
@@ -0,0 +1,25 @@
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+
+import scala.collection.immutable.IndexedSeq
+
+class Taxi8Test extends FunSuite with BeforeAndAfterEach with ShouldMatchers {
+  test("limit for maxk"){
+    assert(Taxi8.calculateMaxK(9, 1) == 3)
+  }
+
+  test("content of sums"){
+    val n = 9
+    val kRange = Range(0, Taxi8.calculateMaxK(n, 1))
+    val nRange = kRange.map(9 + _)
+    val inputPairs = for (x <- nRange; y <- 1 until (x+1)) yield (x,y)
+
+    val values = inputPairs.map(Taxi8.performSumOfCubes)
+    assert(values.contains(BigInt(9*9*9*2)))
+    assert(values.count(b => BigInt(1729) == b) == 2)
+
+    // should be able to get this too
+    //    assert(values.max < BigInt(10*10*10*2))
+    //    assert(values.min == BigInt(9*9*9*2))
+  }
+}
