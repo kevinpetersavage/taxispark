e54503473a93fe57412a4771c6073647a34692ba
Kevin Savage <kevin@Jos-Air-2.home>
2015-02-13 22:29:53 +0000

    record what you are doing to s3

--- src/main/scala/Taxi8.scala
+++ src/main/scala/Taxi8.scala
@@ -1,41 +1,38 @@
-import com.google.common.base.Stopwatch
+import java.net.URI
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.{SparkConf, SparkContext}
-import scala.math.sqrt
 
 object Taxi8 {
 
   def main(args: Array[String]): Unit = {
-    val stopwatch = new Stopwatch().start()
-
-    val start = 1
-    val end = args(0).toInt
-
-    val lowerLimitToTN = args(1).toInt
-    val splits = args(2).toInt
-    val m = ((1.0/2.0)*(sqrt(8 * end + 1) +1)).toInt
-    val nValues = (start until m+1).reverse.scanLeft(1){_+_}
-    val nValuesWithIntervals = nValues.zip(nValues.drop(1)).map(withNext => (withNext._1, withNext._2-withNext._1))
+    val lowerLimitToTN = args(0).toInt
+    val splits = args(1).toInt
+    val bucket = args(2)
 
     val conf = new SparkConf().setAppName("taxi").setMaster("local[4]")
     val sc = new SparkContext(conf)
 
-    val results = nValuesWithIntervals.flatMap{ nWithInterval =>
-      val n = nWithInterval._1
-      val interval = nWithInterval._2
+    Stream.from(1).filter(fileDoesntExist(bucket)).foreach{ n =>
+      val interval = 1
       val maxK = calculateMaxK(n, interval)
-      val kRange = sc.parallelize(0 until maxK, splits)
+      val kRange = sc.parallelize((0 until maxK).reverse, splits)
 
       kRange.flatMap(producePairsFor(n, _, interval))
-        .map(performSumOfCubes)
-        .groupBy(identity)
-        .map(group => (group._2.size, group._1))
-        .filter(pair => pair._1 >= lowerLimitToTN)
-        .collect()
-    }.groupBy(_._1).map(pair => (pair._1, pair._2.map(_._2).min))
+          .map(performSumOfCubes)
+          .groupBy(identity)
+          .map(group => (group._2.size, group._1))
+          .filter(pair => pair._1 >= lowerLimitToTN)
+          .saveAsTextFile(pathFrom(bucket, n))
+    }
+  }
 
-    results.foreach(println(_))
+  def pathFrom(bucket: String, n: Int) = s"$bucket/$n/"
 
-    println("time: " + stopwatch)
+  def fileDoesntExist(bucket: String)(n :Int) : Boolean = {
+    val hdfs = FileSystem.get(new URI(bucket), new Configuration())
+    !hdfs.exists(new Path(pathFrom(bucket, n)))
   }
 
   def producePairsFor(n: Int, k: Int, interval: Int): TraversableOnce[(Int,Int)] = {
